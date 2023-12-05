0903953c9f49e42e7e740afc7e3235c3478980ab
Vito Sandrin <sandrinvito10@gmail.com>
2023-12-05 09:38:13 -0300

    Add error handling and logging to the application

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import Application from "./app";
+import logger from "./logger";
+
+enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
+
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error(`App exiting due to an unhandled promise: ${promise} and reason: ${reason}`);
+  throw reason;
+});
+
+process.on("uncaughtException", (error) => {
+  logger.error(`App exiting due to an uncaught exception: ${error}`);
+  process.exit(ExitStatus.Failure);
+});
+
+(async (): Promise<void> => {
+  try {
+    logger.info("Starting app...");
+    try {
+      const app = new Application();
+      await app.run();
+
+      logger.info(`App exited with success`);
+      process.exit(ExitStatus.Success);
+    } catch (error) {
+      logger.error(`App exited with error: ${error}`);
+      process.exit(ExitStatus.Failure);
+    }
+  } catch (error) {
+    logger.error(`App exited with error: ${error}`);
+    process.exit(ExitStatus.Failure);
+  }
+})();
