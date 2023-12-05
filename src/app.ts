3f3b5ddd44b4a754c7e31cafa5bd73decaddaac4
Vito Sandrin <sandrinvito10@gmail.com>
2023-12-05 09:38:18 -0300

    Add application class and CLI functionality

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import figlet from "figlet";
+import { Command } from "commander";
+import logger from "./logger";
+
+class Application {
+  private program: Command;
+  constructor() {
+    this.program = new Command();
+  }
+
+  public async execute() {
+    try {
+      console.log("Hello Program");
+    } catch (error) {
+      logger.error(error);
+    }
+  }
+
+  public async run() {
+    console.info(figlet.textSync("I am a CLI"));
+    this.program.version("1.0.0");
+    try {
+      await this.execute();
+    } catch (error) {
+      logger.error(error);
+    }
+  }
+}
+export default Application;
